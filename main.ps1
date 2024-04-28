143b284a382536f87ab70435136e8f3fe1c906f3
Muzaffar Sharofitdinov <smartboymuzaffar@gmail.com>
2024-04-28 22:35:17 +0900

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,3 +1,3 @@
-Start-Process pythonw C:\Users\smart\AppData\Roaming\client.pyw
+Start-Process pythonw C:\Users\$env:USERNAME\AppData\Roaming\client.pyw
 # C:\Users\smart\AppData\Local
 # C:\Users\smart\AppData\Roaming
